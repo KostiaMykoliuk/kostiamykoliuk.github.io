6591eb952f9bef4605854bf3b0f17540225d42e4
Kostia Mykoliuk <playnick@ukr.net>
2021-06-02 12:30:58 +0300

    temporary view of the site

---
+++ assets/script/main.js
@@ -0,0 +1,29 @@
+var siteIstotope = function() {
+    var $container = $('#portfolio-grid').isotope({
+      itemSelector: '.item',
+      isFitWidth: true
+    });
+
+    $(window).resize(function() {
+      $container.isotope({
+        columnWidth: '.col-sm-3'
+      });
+    });
+
+    $container.isotope({
+      filter: '*'
+    });
+
+    $('#filters').on('click', 'a', function(e) {
+      e.preventDefault();
+      var filterValue = $(this).attr('data-filter');
+      $container.isotope({
+        filter: filterValue
+      });
+      $('#filters a').removeClass('active');
+      $(this).addClass('active');
+    });
+  }
+  $(window).on('load', function() {
+    siteIstotope();
+  });
